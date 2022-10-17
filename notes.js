4ab62eb6f98aaf08204f223385a0158a341d4fcf
Onkar Galande <73571109+galandeonkar@users.noreply.github.com>
2022-10-17 23:45:56 +0530

    Add files via upload

---
+++ notes.js
@@ -0,0 +1,73 @@
+const fs = require("fs");
+const chalk = require("chalk").default;
+
+const addNote = (title, body) => {
+  const notes = loadNotes();
+  // const duplicateNotes = notes.filter((note) => note.title === title);
+  const duplicateNote = notes.find((note) => note.title === title);
+
+  if (!duplicateNote) {
+    notes.push({
+      title: title,
+      body: body,
+    });
+    saveNotes(notes);
+    console.log(chalk.green.inverse("New note added!"));
+  } else {
+    console.log(chalk.red.inverse("Note titel already taken!"));
+  }
+};
+
+const removeNote = (title) => {
+  const notes = loadNotes();
+  const notesToKeep = notes.filter((note) => note.title !== title);
+  if (notes.length > notesToKeep.length) {
+    console.log(chalk.green.inverse("Note removed sucessfully!"));
+    saveNotes(notesToKeep);
+  } else {
+    console.log(chalk.red.inverse("No Note found!"));
+  }
+};
+
+const listNotes = () => {
+  const notes = loadNotes();
+  // const notesList = notes.filter((note) => note.title);
+  console.log(chalk.inverse("Your Notes"));
+  notes.forEach((note) => {
+    console.log(note.title);
+  });
+};
+
+const readNote = (title) => {
+  const notes = loadNotes();
+  const note = notes.find((note) => note.title === title);
+
+  if (note) {
+    console.log(chalk.inverse(note.title));
+    console.log(note.body);
+  } else {
+    console.log(chalk.red.inverse("Note not found!"));
+  }
+};
+
+const saveNotes = (notes) => {
+  const dataJSON = JSON.stringify(notes);
+  fs.writeFileSync("notes.json", dataJSON);
+};
+
+const loadNotes = () => {
+  try {
+    const dataBuffer = fs.readFileSync("notes.json");
+    const dataJSON = dataBuffer.toString();
+    return JSON.parse(dataJSON);
+  } catch (error) {
+    return [];
+  }
+};
+
+module.exports = {
+  addNote: addNote,
+  removeNote: removeNote,
+  listNotes: listNotes,
+  readNote: readNote,
+};
